c07150d3ca65630584dda61486a7dfc49516e16f
kny2383 <kny2383@gmail.com>
2025-05-14 15:23:43 +0900

    미션 관련 API 응답 통일 & 에러 핸들링

--- homework/src/controllers/mission.controller.js
+++ homework/src/controllers/mission.controller.js
@@ -8,30 +8,21 @@ import { updateMissionStatus } from "../services/mission.service.js";
 export const handleCreateMission = async (req, res) => {
   const { store_id } = req.params;
 
-  try {
-    // body + store_id → Mission 데이터 구성
-    const mission = bodyToMission(req.body, store_id);
+  // body + store_id → Mission 데이터 구성
+  const mission = bodyToMission(req.body, store_id);
 
-    // 서비스 로직 호출
-    const result = await createMission(mission);
+  // 서비스 로직 호출
+  const result = await createMission(mission);
 
-    // 응답 포맷으로 변환하여 전송
-    res.status(201).json(responseFromMission(result));
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  res.status(StatusCodes.OK).success(result);
 };
 
 export const handleChallengeMission = async (req, res) => {
     const { mission_id } = req.params;
     const { user_id } = req.body;
-  
-    try {
-      const result = await challengeMission({ mission_id: Number(mission_id), user_id });
-      res.status(201).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
+
+    const result = await challengeMission({ mission_id: Number(mission_id), user_id });
+    res.status(StatusCodes.OK).success(result);
   };
 
 export const handleListUserMissions = async (req, res, next) => {
@@ -40,16 +31,12 @@ export const handleListUserMissions = async (req, res, next) => {
       const cursor = typeof req.query.cursor === "string" ? parseInt(req.query.cursor) : undefined;
 
       const userMissions = await listUserMissions(userId, status, cursor);
-      res.status(StatusCodes.OK).json({ userMissions });
+      res.status(StatusCodes.OK).success(userMissions);
 };
 
 export const handleCompleteUserMission = async (req, res) => {
   const { userId, missionId } = req.params;
-
-  try {
-    await updateMissionStatus(userId, missionId);
-    res.status(StatusCodes.OK).json({ message: "미션 완료 처리됨" });
-  } catch (err) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
-  }
+  const result = await updateMissionStatus(userId, missionId);
+  
+  res.status(StatusCodes.OK).success(result);
 };
--- homework/src/services/mission.service.js
+++ homework/src/services/mission.service.js
@@ -6,28 +6,31 @@ import {
   getUserMissions,
   completeUserMission,
 } from '../repositories/mission.repository.js';
-
 import { findOwnerByStoreId } from '../repositories/owner.repository.js';
 import { responseFromUserMissions } from '../dtos/mission.dto.js';
-
-
+import { 
+  MatchToStoreError,
+  DuplicateMissionError,
+  ExistMissionError,
+  CompleteMissionError
+} from "../errors.js";
 
   export const createMission = async (data) => {
     const { store_id, point, deadline, mission_detail } = data;
   
-    // 1. store_id로 owner_number 조회
     const owner_number = await findOwnerByStoreId(store_id);
+    
     if (!owner_number) {
-      throw new Error('해당 store_id에 대한 owner가 존재하지 않습니다.');
+      throw new MatchToStoreError('해당 store가 없습니다.', data);
     }
-  
+
     const missionId = await addMission({
       store_id,
       point,
       deadline,
       mission_detail,
     });
-  
+
     return {
       mission_id: missionId,
       store_id,
@@ -40,15 +43,13 @@ import { responseFromUserMissions } from '../dtos/mission.dto.js';
 
   export const challengeMission = async ({ mission_id, user_id }) => {
     const mission = await findMissionById(mission_id);
-    if (!mission) {
-      throw new Error('해당 미션이 존재하지 않습니다.');
-    }
-  
+
     const existingChallenge = await findUserMission({ mission_id, user_id });
+    
     if (existingChallenge) {
-      throw new Error('이미 도전 중인 미션입니다.');
+      throw new DuplicateMissionError('이미 도전 중인 미션입니다.', { user_mission_id: existingChallenge.id });
     }
-  
+    
     const userMissionId = await addUserMission({
         mission_id,
         user_id,
@@ -59,19 +60,36 @@ import { responseFromUserMissions } from '../dtos/mission.dto.js';
           connect: { id: mission_id }  
         }
       });
-      
-  
+
     return { message: '미션 도전이 성공적으로 등록되었습니다.', user_mission_id: userMissionId };
   };
 
-export const listUserMissions = async (userId, status, cursor) => {
-  const missions = await getUserMissions(userId, status, cursor);
+export const listUserMissions = async (user_id, status, cursor) => {
+  const missions = await getUserMissions(user_id, status, cursor);
+
+  const existingChallenge = await findUserMission({user_id: Number(user_id)});
+
+  if (!existingChallenge) {
+    throw new ExistMissionError('해당 유저의 미션이 존재하지 않습니다.')
+  }
   return responseFromUserMissions(missions);
 };
 
-export const updateMissionStatus = async (userId, missionId) => {
-  const updated = await completeUserMission(userId, missionId);
-  if (!updated) {
-    throw new Error("해당 미션이 존재하지 않거나 이미 완료됨");
+export const updateMissionStatus = async (user_id, mission_id) => {
+  const updated = await completeUserMission(user_id, mission_id);
+
+  const existingChallenge = await findUserMission({
+  mission_id: Number(mission_id),
+  user_id: Number(user_id),
+});
+  console.log(existingChallenge)
+  if (mission_id != existingChallenge.missionId) { 
+    throw new ExistMissionError('해당 유저의 미션이 존재하지 않습니다.')
   }
+
+  if (!updated) {
+    throw new CompleteMissionError("해당 미션이 이미 완료되었습니다.");
+  }
+
+  return { message: '미션 완료로 상태를 변경하였습니다' };
 };
