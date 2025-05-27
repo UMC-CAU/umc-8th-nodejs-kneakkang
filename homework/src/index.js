1435147bd45579d8c17fefc88bd8fb59c07c0f34
kny2383 <kny2383@gmail.com>
2025-05-28 01:28:23 +0900

    [Feature] 네이버 로그인 구현

--- homework/src/index.js
+++ homework/src/index.js
@@ -11,8 +11,20 @@ import { handleChallengeMission } from "./controllers/mission.controller.js";
 import { handleListStoreReviews } from "./controllers/store.controller.js";
 import { handleListUserMissions } from "./controllers/mission.controller.js";
 import { handleCompleteUserMission } from "./controllers/mission.controller.js";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import session from "express-session";
+import passport from "passport";
+import { googleStrategy } from "./auth.config.js";
+import { naverStrategy } from "./auth.config.js";
+import { prisma } from "./db.config.js";
 
 dotenv.config();
+
+passport.use(googleStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+passport.use(naverStrategy);
+
 
 const app = express();
 const port = process.env.PORT;
@@ -41,7 +53,27 @@ app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", (req, res) => {
+  console.log(req.user);
   res.send("Hello World!");
 });
 
@@ -72,15 +104,46 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.use(
-  "/docs",
-  swaggerUiExpress.serve,
-  swaggerUiExpress.setup({}, {
-    swaggerOptions: {
-      url: "/openapi.json",
-    },
-  })
+// 자동으로 Google 로그인 주소로 이동하여 사용자가 Google 로그인을 할 수 있도록 함
+app.get("/oauth2/login/google", passport.authenticate("google"));
+// Google 로그인이 성공하면 자동으로 되돌아오는 주소
+// 쿼리 파라미터로 전달된 code 값을 이용해 Google API를 호출하여 사용자의 프로필 정보를 조회해오고, 이를 Session에 저장
+app.get(
+  "/oauth2/callback/google",
+  passport.authenticate("google", {
+    failureRedirect: "/oauth2/login/google",
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect("/")
 );
+
+passport.serializeUser((user, done) => done(null, user.email));
+passport.deserializeUser(async (email, done) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+  done(null, user);
+});
+
+app.get("/oauth2/login/naver", passport.authenticate("naver"));
+
+app.get(
+  "/oauth2/callback/naver",
+  passport.authenticate("naver", {
+    failureRedirect: "/oauth2/login/naver",
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect("/")
+);
+
+app.get("/oauth2/profile", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json({
+      message: "로그인 성공",
+      user: req.user,
+    });
+  } else {
+    res.status(401).json({ message: "로그인이 필요합니다." });
+  }
+});
 
 app.get("/openapi.json", async (req, res, next) => {
   // #swagger.ignore = true
